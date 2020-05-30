9ea75aed995e02579b2447b514c811b80b2fbee7
Linda McGraw <linda.mcgraw@me.com>
2020-05-30 13:29:46 -0500

    webservice added

--- vuebnb/routes/api.php
+++ vuebnb/routes/api.php
@@ -1,21 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\Listing;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::get('listing/{listing}', function(Listing $listing){
-    return $listing->toJson();
-});
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('/listing', 'ListingController@index');
+Route::get('/listing/{listing}', 'ListingController@get_listing_api');
