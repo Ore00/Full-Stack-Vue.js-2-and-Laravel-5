a185084e1494d9dd69a1bcc1d015254dc1462e58
Linda McGraw <linda.mcgraw@me.com>
2020-06-07 22:45:13 -0500

    carousel controls added

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -6,22 +6,32 @@ let model = JSON.parse(window.vuebnb_listing_model);
 model = populateAmenitiesAndPrices(model);
 Vue.component('image-carousel', {
     template: `<div class="image-carousel">
-                 <img v-bind:src="images[index]"/>
-            </div>`,
+                    <img :src="image"/>
+                    <div class="controls">
+                        <carousel-control dir="left"></carousel-control>
+                        <carousel-control dir="right"></carousel-control>
+                    </div>
+                </div>`,
+    props: ['images'],
     data(){
         return{
-            images: [
-                '/images/1/Image_1.jpg',
-                '/images/1/Image_2.jpg',
-                '/images/1/Image_3.jpg',
-                '/images/1/Image_4.jpg',
-            ],
             index: 0
         }
     },
     computed: {
         image(){
             return this.images[this.index];
+        }
+    },
+    components: {
+        'carousel-control': {
+            template: `<i :class="classes"></i>`,
+            props: [ 'dir' ],
+            computed: {
+                classes(){
+                    return 'carousel-control fa fa-2x fa-chevron-' + this.dir;
+                }
+            }
         }
     }
 });
