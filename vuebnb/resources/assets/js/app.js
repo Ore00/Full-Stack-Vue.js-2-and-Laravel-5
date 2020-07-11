0a05cdc3d78c6a50ecc6f3f833683422ec5d2465
Linda McGraw <linda.mcgraw@me.com>
2020-07-11 09:19:25 -0500

    ch7 flex and rows for listings

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -1,8 +1,12 @@
 import "core-js/fn/object/assign";
 import Vue from 'vue';
 
-import ListingPage from '../components/ListingPage.vue';
+
+import router from './router.js';
+import App from '../components/App.vue';
+
 var app = new Vue({
   el: '#app',
-  render: h => h(ListingPage)
+  render: h => h(App),
+  router
 });
